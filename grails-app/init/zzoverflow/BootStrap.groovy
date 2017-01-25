6fc9d9f8cf80c898d246ef54b5f3a9dbcfa3bc6f
ousmane TRAORE <tra.ousman@gmail.com>
2017-01-25 16:02:46 +0100

    Add some badges add bootstrap

--- grails-app/init/zzoverflow/BootStrap.groovy
+++ grails-app/init/zzoverflow/BootStrap.groovy
@@ -39,27 +39,32 @@ class BootStrap {
             it.flush()
             it.clear()
         }
-                        
+        def questionBadge1       = new Badge(name : 'Nice Question').save()
+        def questionBadge2       = new Badge(name : 'Famous Question').save()  
         def question = new Question(
                                 title : '10 Kids Unaware of Their Halloween Costume', 
                                 body :  'Today, were looking at three particularly interesting stories. Pinterest added a new location-based feature on Wednesday that uses Place Pins as a way to map out vacations and favorite areas. Southwest Airlines is providing Wi-Fi access from gate to gate for 8 per day through an onboard hotspot. And in an effort to ramp up its user base, Google Wallet is offering a debit card that can take out cash from.', 
-                                nbView : 1, 
-                                upvote : 0,
+                                nbView : 200000, 
+                                upvote : 15,
                                 downvote : 0)
                                 .addToTags(tag)
 
         question.setUser(user)   
-        question.save()                     
+        question.save() 
+
+        QuestionBadge.create question, questionBadge1
+        QuestionBadge.create question, questionBadge2           
 
         def question2 = new Question(
                                 title : 'What Instagram Ads Will Look Like, What Other social networks Ads Will Look Like', 
                                 body :  "Today, we're looking at three particularly interesting stories. Pinterest added a new location-based feature on Wednesday that uses Place Pins as a way to map out vacations and favorite areas. Southwest Airlines is providing Wi-Fi access from gate to gate for 8 per day through an onboard hotspot. And in an effort to ramp up its user base, Google Wallet is offering a debit card that can take out cash from.",
                                 nbView : 1, 
-                                upvote : 0,
+                                upvote : 11,
                                 downvote : 0)
                                 .addToTags(tag2)
         question2.setUser(user) 
-        question2.save()                    
+        question2.save() 
+        QuestionBadge.create question2, questionBadge1              
 
         def question3 = new Question(
                                 title : 'The Future of Magazines Is on Tablets', 
@@ -71,15 +76,16 @@ class BootStrap {
         question3.setUser(user)
         question3.save() 
                                                                                   
-
+        def answerBadge1       = new Badge(name : 'Great Answer').save()
         def answer   = new Answer(
                                 body : 'This is the solution body !!!', 
-                                upvote : 0, 
-                                downvote : 0,
+                                upvote : 200, 
+                                downvote : 5,
                                 question : question)
         answer.setUser(user)                 
         answer.save() 
-        question.setCorrect(answer)            
+        question.setCorrect(answer)
+        AnswerBadge.create answer, answerBadge1         
 
     }
 
